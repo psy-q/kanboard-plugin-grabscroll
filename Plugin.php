f7c7688a6864541719881b116c3e28823b0c91df
Ramon Cahenzli <rca@psy-q.ch>
2019-09-05 13:43:00 +0200

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Kanboard\Plugin\GrabScroll;
+
+use Kanboard\Core\Plugin\Base;
+
+class Plugin extends Base
+{
+    public function initialize()
+    {
+        $this->hook->on('template:layout:js', array('template' => 'plugins/GrabScroll/Assets/scroll.js'));
+    }
+
+    public function onStartup()
+    {
+    }
+
+    public function getPluginName()
+    {
+        return 'GrabScroll';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('Scroll a board horizontally by grabbing it.');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'Ramón Cahenzli with code by ToddWebDev';
+    }
+
+    public function getPluginVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/psy-q/kanboard-plugin-grabscroll';
+    }
+
+    public function getCompatibleVersion()
+    {
+        return '>1.2.10';
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+GrabScroll
+==========
+
+Click and drag anywhere on your Kanboard to scroll it.
+
+Author
+------
+
+- Ramón Cahenzli (with code by ToddWebDev)
+- License MIT
+
+Requirements
+------------
+
+- Kanboard > 1.2.10
+
+Installation
+------------
+
+Download the zip file and decompress everything under the directory `plugins/GrabScroll`
+
+Note: Plugin folder is case-sensitive.
